e71a7a810ab12a3114fe95d17d06e47663545d9f
Tanmoy Debnath <extanmoy0@gmail.com>
2026-07-19 08:49:10 +0600

    task1 added

---
+++ day-1/task-1.js
@@ -0,0 +1,6 @@
+let massage1 = "Hello JavaScript!" ;
+let massage2 = "My name is Tanmoy." ;
+let massage3 = "I am excited to become a Web Developer.";
+console.log(massage1);
+console.log(massage2);
+console.log(massage3);
