c2f4fc02b93085fcdc2ddaef95460b116f57d21a
Tanmoy Debnath <extanmoy0@gmail.com>
2026-07-21 19:30:37 +0600

    Day 3 task

---
+++ day-3/task-1.js
@@ -0,0 +1,118 @@
+// 01
+
+// for(let i = 0; i < 5; i++){
+//     console.log('Hello');
+// }
+
+// 02
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+// 03
+
+// let i = 10;
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+// 04
+
+// let i = 1;
+// while(i <= 10){
+//     console.log('Loop চলছে', i);
+//     i++ ;
+// }
+
+// 05
+
+// for(let i = 0; i <= 30; i += 2){
+//     console.log(i);
+// }
+
+// 06
+
+// let sum = 0;
+// for(let i = 1; i <= 20; i++){
+//     console.log(sum, '+', i, '=', sum = sum + i) ;
+// }
+// console.log('So Finally the Total Sum is =' , sum);
+
+// 07
+
+// let n = 8;
+// for(i = 1; i <= 10; i++){
+//     console.log(n, 'x', i, '=', n * i);
+// }
+
+// 08
+
+// for(let i = 20; i >= 1; i--){
+//     console.log(i);
+// }
+
+//09
+
+// let sum = 0;
+// for(i = 2; i <= 50; i += 2){
+//     console.log(sum, '+', i, '=', sum = sum + i);
+// }
+// console.log('Total Sum Of Even Numbers:', sum);
+
+// 10
+
+// for(i = 1; i <= 50; i++){
+//     if(i > 30){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// 11
+
+// for(i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// 12 (do while)
+
+// let i = 10;
+// do{
+//     console.log(i);
+//     i++ ;
+// } while(i <= 5);
+
+
+// 13
+
+// for(let i = 1; i <=5; i++){         // For loop
+//     console.log(i);
+// }
+
+// let i = 1;             // While
+// while(i <= 5){
+//     console.log(i);
+//     i++ ;
+// }
+
+// let i = 1;        // Do While
+// do{
+//     console.log(i);
+//     i++ ;
+// } while(i <= 5);
+
+
+// 14
+
+// for(let i = 1; i <= 100; i++){
+//     if (i > 40) {
+//         break;
+//     }if(i % 5 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
